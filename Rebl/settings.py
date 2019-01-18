a86d347a62bd450490223e629f3f27aed3fbbe51
kai topaloglu <hakan.topaloglu@mail.utoronto.ca>
2019-01-18 02:41:17 -0500

    updated .gitignore

--- Rebl/settings.py
+++ Rebl/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 # doesnt recognize  packages/3rd party apps installed with pipenv????why?
 import django_heroku
-
+from .settings_secret import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9-^%nwdc&8uz&7incq3$_0h776b=snpzem5b8!eeacz%iz@#m('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,18 +148,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
-# stripe settings
-STRIPE_PUBLISHABLE_KEY = 'pk_test_chgSsG2gRzRwu6HP7L92Z0ph'
-STRIPE_SECRET_KEY = 'sk_test_bvXRVKS8lvATZky07xhvXUHD'
 
-# mailgun settings
-EMAIL_HOST = 'smtp.mailgun.org'
-# secure port that uses TLS encryption
-EMAIL_PORT = '587'
-# since using this type of encryption
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'postmaster@sandboxe5d1dc112fc34991a23b5db05172fa28.mailgun.org'
-EMAIL_HOST_PASSWORD = '35bb7d393e4158dfc47e6e4681fc67cf-3939b93a-96a70cfe'
 
 # Image_fit settings
 IMAGEFIT_ROOT = os.path.join(BASE_DIR, 'static')
