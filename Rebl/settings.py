3d8833b7de92a7cc3a0165106428d0a8d623c843
kai topaloglu <hakan.topaloglu@mail.utoronto.ca>
2019-01-21 00:52:39 -0500

    configured settings

--- Rebl/settings.py
+++ Rebl/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 # doesnt recognize  packages/3rd party apps installed with pipenv????why?
 import django_heroku
-from .settings_secret import *
+#from .settings_secret import *
 import datetime
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -219,3 +219,17 @@ IMAGEFIT_ROOT = os.path.join(BASE_DIR, 'static')
 #configure django app for heroku
 django_heroku.settings(locals())
 
+# stripe settings
+STRIPE_PUBLISHABLE_KEY = 'pk_test_chgSsG2gRzRwu6HP7L92Z0ph'
+STRIPE_SECRET_KEY = 'sk_test_bvXRVKS8lvATZky07xhvXUHD'
+
+# mailgun settings
+EMAIL_HOST = 'smtp.mailgun.org'
+# secure port that uses TLS encryption
+EMAIL_PORT = '587'
+# since using this type of encryption
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'postmaster@sandboxe5d1dc112fc34991a23b5db05172fa28.mailgun.org'
+EMAIL_HOST_PASSWORD = '35bb7d393e4158dfc47e6e4681fc67cf-3939b93a-96a70cfe'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '9-^%nwdc&8uz&7incq3$_0h776b=snpzem5b8!eeacz%iz@#m('
